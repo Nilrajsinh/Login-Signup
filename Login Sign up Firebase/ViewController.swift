d412ab0a0038efb5b744479f1a29e16943454582
Nilrajsinh <Nilrajsinh1@icloud.com>
2019-12-28 13:44:32 +0530

    Initial Commit

---
+++ Login Sign up Firebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login Sign up Firebase
+//
+//  Created by Nilrajsinh Vaghela on 28/12/19.
+//  Copyright © 2019 Nilrajsinh Vaghela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
